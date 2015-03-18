f75149bcb5161bd3fb41d0b2af86cf7b972d1b16
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-18 19:35:54 +0100

    Initialize project with leiningen
    The command used is `lein new app sprite-mapper`

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sprite-mapper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sprite_mapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns sprite-mapper.core-test
+  (:require [clojure.test :refer :all]
+            [sprite-mapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
